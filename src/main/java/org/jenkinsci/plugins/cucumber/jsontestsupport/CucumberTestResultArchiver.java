d39c672e4161c9e586238c482a692c2899679a33
Tetiana Pidhurska <tetiana.pidhurska@travisperkins.co.uk>
2017-07-07 10:59:40 +0300

    Applied Patch

--- src/main/java/org/jenkinsci/plugins/cucumber/jsontestsupport/CucumberTestResultArchiver.java
+++ src/main/java/org/jenkinsci/plugins/cucumber/jsontestsupport/CucumberTestResultArchiver.java
@@ -33,14 +33,9 @@ import hudson.matrix.MatrixAggregator;
 import hudson.matrix.MatrixBuild;
 import hudson.model.Action;
 import hudson.model.BuildListener;
-import hudson.model.CheckPoint;
-import hudson.model.Job;
-import hudson.model.Project;
 import hudson.model.Result;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.Run;
-import hudson.model.TaskListener;
 import hudson.remoting.Callable;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
@@ -49,7 +44,6 @@ import hudson.tasks.Recorder;
 import hudson.tasks.test.TestResultAggregator;
 import hudson.tasks.test.TestResultProjectAction;
 import hudson.util.FormValidation;
-import jenkins.security.MasterToSlaveCallable;
 
 import java.io.File;
 import java.io.IOException;
@@ -57,18 +51,13 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.logging.Logger;
 
-import jenkins.tasks.SimpleBuildStep;
 import net.sf.json.JSONObject;
 
 import org.apache.tools.ant.types.FileSet;
-import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
-
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
 import org.kohsuke.stapler.DataBoundSetter;
 
 /**
@@ -77,7 +66,7 @@ import org.kohsuke.stapler.DataBoundSetter;
  * @author James Nord
  * @author Kohsuke Kawaguchi (original JUnit code)
  */
-public class CucumberTestResultArchiver extends Recorder implements MatrixAggregatable, SimpleBuildStep {
+public class CucumberTestResultArchiver extends Recorder implements MatrixAggregatable {
 	private static final Logger LOGGER = Logger.getLogger(CucumberTestResultArchiver.class.getName());
 
 	/**
@@ -106,23 +95,9 @@ public class CucumberTestResultArchiver extends Recorder implements MatrixAggreg
 		return ignoreBadSteps;
 	}
 
-    @Override
-    @SuppressFBWarnings(value={"NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE"}, justification="whatever")
-    public boolean
-    perform(AbstractBuild build, Launcher launcher, BuildListener listener) throws InterruptedException,
-            IOException {
-        return publishReport(build, build.getWorkspace(), launcher, listener);
-    }
-
-
 	@Override
-	public void perform(Run<?, ?> run, FilePath filePath, Launcher launcher, TaskListener taskListener) throws InterruptedException, IOException {
-		publishReport(run, filePath, launcher, taskListener);
-	}
-
-	@SuppressFBWarnings(value={"RV_RETURN_VALUE_IGNORED_BAD_PRACTICE"}, justification="move to java.nio for file stuff")
 	public boolean
-	      publishReport(Run<?, ?> build, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException,
+	      perform(AbstractBuild build, Launcher launcher, BuildListener listener) throws InterruptedException,
 	                                                                             IOException {
 		// listener.getLogger().println(Messages.JUnitResultArchiver_Recording());
 
@@ -130,57 +105,62 @@ public class CucumberTestResultArchiver extends Recorder implements MatrixAggreg
 
 		final String _testResults = build.getEnvironment(listener).expand(this.testResults);
 
-		CucumberJSONParser parser = new CucumberJSONParser(ignoreBadSteps);
+		try {
+			CucumberJSONParser parser = new CucumberJSONParser(ignoreBadSteps);
 
-		CucumberTestResult result = parser.parseResult(_testResults, build, workspace, launcher, listener);
+			CucumberTestResult result = parser.parse(_testResults, build, launcher, listener);
 
-		// TODO - look at all of the Scenarios and see if there are any embedded items contained with in them
-		String remoteTempDir = launcher.getChannel().call(new TmpDirCallable());
+			// TODO - look at all of the Scenarios and see if there are any embedded items contained with in them
+			String remoteTempDir = launcher.getChannel().call(new TmpDirCallable());
 
-		// if so we need to copy them to the master.
-		for (FeatureResult f : result.getFeatures()) {
-			for (ScenarioResult s : f.getScenarioResults()) {
-				for (EmbeddedItem item : s.getEmbeddedItems()) {
-					// this is the wrong place to do the copying...
-					// XXX Need to do something with MasterToSlaveCallable to makesure we are safe from evil
-					// injection
-					FilePath srcFilePath = new FilePath(launcher.getChannel(), remoteTempDir + '/' + item.getFilename());
-					// XXX when we support the workflow we will need to make sure that these files do not clash....
-					File destRoot = new File(build.getRootDir(), "/cucumber/embed/" + f.getSafeName() + '/' + s
-							.getSafeName() + '/');
-					destRoot.mkdirs();
-					File destFile = new File(destRoot, item.getFilename());
-					if (!destFile.getAbsolutePath().startsWith(destRoot.getAbsolutePath())) {
-						// someone is trying to trick us into writing abitrary files...
-						throw new IOException("Exploit attempt detected - Build attempted to write to " +
-								destFile.getAbsolutePath());
+			// if so we need to copy them to the master.
+			for (FeatureResult f : result.getFeatures()) {
+				for (ScenarioResult s : f.getScenarioResults()) {
+					for (EmbeddedItem item : s.getEmbeddedItems()) {
+						// this is the wrong place to do the copying...
+						// XXX Need to do something with MasterToSlaveCallable to makesure we are safe from evil
+						// injection
+						FilePath srcFilePath = new FilePath(launcher.getChannel(), remoteTempDir + '/' + item.getFilename());
+						// XXX when we support the workflow we will need to make sure that these files do not clash....
+						File destRoot = new File(build.getRootDir(), "/cucumber/embed/" + f.getSafeName() + '/' + s
+								.getSafeName() + '/');
+						destRoot.mkdirs();
+						File destFile = new File(destRoot, item.getFilename());
+						if (!destFile.getAbsolutePath().startsWith(destRoot.getAbsolutePath())) {
+							// someone is trying to trick us into writing abitrary files...
+							throw new IOException("Exploit attempt detected - Build attempted to write to " +
+									destFile.getAbsolutePath());
+						}
+						FilePath destFilePath = new FilePath(destFile);
+						srcFilePath.copyTo(destFilePath);
+						srcFilePath.delete();
 					}
-					FilePath destFilePath = new FilePath(destFile);
-					srcFilePath.copyTo(destFilePath);
-					srcFilePath.delete();
 				}
 			}
-		}
-		
-		action = build.getAction(CucumberTestResultAction.class);
-		
-		if (action == null) {
+			
 			action = new CucumberTestResultAction(build, result, listener);
-			CHECKPOINT.block();
-			//build.addAction(action);
-			CHECKPOINT.report();
-		}
-		else {
-			CHECKPOINT.block();
-			action.mergeResult(result, listener);
-			build.save();
-			CHECKPOINT.report();
-		}
-		// action.setHealthScaleFactor(getHealthScaleFactor()); // overwrites previous value if appending
-		
 
-		if (result.getPassCount() == 0 && result.getFailCount() == 0 && result.getSkipCount() == 0)
-			throw new AbortException("No cucumber scenarios appear to have been run.");
+			if (result.getPassCount() == 0 && result.getFailCount() == 0 && result.getSkipCount() == 0)
+				throw new AbortException("No cucumber scenarios appear to have been run.");
+
+		}
+		catch (AbortException e) {
+			if (build.getResult() == Result.FAILURE) {
+				// most likely a build failed before it gets to the test phase.
+				// don't report confusing error message.
+				return true;
+			}
+			listener.getLogger().println(e.getMessage());
+			build.setResult(Result.FAILURE);
+			return true;
+		}
+		catch (IOException e) {
+			e.printStackTrace(listener.error("Failed to archive cucumber reports"));
+			build.setResult(Result.FAILURE);
+			return true;
+		}
+
+		build.getActions().add(action);
 
 		if (action.getResult().getTotalCount() == action.getResult().getFailCount()){
 			build.setResult(Result.FAILURE);
@@ -207,9 +187,9 @@ public class CucumberTestResultArchiver extends Recorder implements MatrixAggreg
 
 	@Override
 	public Collection<Action> getProjectActions(AbstractProject<?, ?> project) {
-		return Collections.<Action> singleton(new TestResultProjectAction((Job)project));
+		return Collections.<Action> singleton(new TestResultProjectAction(project));
 	}
-	
+
 
 	public MatrixAggregator createAggregator(MatrixBuild build, Launcher launcher, BuildListener listener) {
 		return new TestResultAggregator(build, launcher, listener);
@@ -218,8 +198,6 @@ public class CucumberTestResultArchiver extends Recorder implements MatrixAggreg
 	/**
 	 * Test result tracks the diff from the previous run, hence the checkpoint.
 	 */
-	private static final CheckPoint CHECKPOINT = new CheckPoint("Cucumber result archiving");
-
 	private static final long serialVersionUID = 1L;
 
 
@@ -231,12 +209,12 @@ public class CucumberTestResultArchiver extends Recorder implements MatrixAggreg
 	/**
 	 * {@link Callable} that gets the temporary directory from the node. 
 	 */
-	private final static class TmpDirCallable extends MasterToSlaveCallable<String, InterruptedException> {
+	private final static class TmpDirCallable implements Callable<String, InterruptedException> {
 
 		private static final long serialVersionUID = 1L;
 
 		@Override
-		public String call() {
+		public String call() throws InterruptedException {
 			return System.getProperty("java.io.tmpdir");
 		}
 	}
@@ -244,7 +222,6 @@ public class CucumberTestResultArchiver extends Recorder implements MatrixAggreg
 
 
 	@Extension
-	@Symbol("cucumber")
 	public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
 		public String getDisplayName() {
@@ -266,10 +243,7 @@ public class CucumberTestResultArchiver extends Recorder implements MatrixAggreg
 		 */
 		public FormValidation doCheckTestResults(@AncestorInPath AbstractProject project,
 		                                         @QueryParameter String value) throws IOException {
-			if (project != null) {
-				return FilePath.validateFileMask(project.getSomeWorkspace(), value);
-			}
-			return FormValidation.ok(); 
+			return FilePath.validateFileMask(project.getSomeWorkspace(), value);
 		}
 
 
