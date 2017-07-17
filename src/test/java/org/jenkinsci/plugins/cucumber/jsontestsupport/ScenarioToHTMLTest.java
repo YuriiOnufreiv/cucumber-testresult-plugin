5f6cca6f3163eeef1629b371953ca6b5240e354e
Tetiana Pidhurska <tetiana.pidhurska@travisperkins.co.uk>
2017-07-17 12:02:13 +0300

    Added unit tests to check addBeforeAfterResult calls addDuration correctly

--- src/test/java/org/jenkinsci/plugins/cucumber/jsontestsupport/ScenarioToHTMLTest.java
+++ src/test/java/org/jenkinsci/plugins/cucumber/jsontestsupport/ScenarioToHTMLTest.java
@@ -1,23 +1,30 @@
 package org.jenkinsci.plugins.cucumber.jsontestsupport;
 
+import gherkin.formatter.model.Match;
+import gherkin.formatter.model.Result;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class ScenarioToHTMLTest {
 
     private static final String PASSED_STEP_CSS = "background-color: #e6ffcc;";
+    private static final String SKIPPED_STEP_CSS = "background-color: #ffffcc;";
 
     private static final float DURATION = 1.5f;
     private static final float INVALID_DURATION = 2f;
+    private static final String EMPTY_BEFORE_OR_AFTER = "";
+
     private static final String EXPECTED_ADD_DURATION_HTML_FOR_PASSED_STEP =
             "<td nowrap=\"nowrap\" valign=\"top\" align=\"left\" style=\"background-color: #e6ffcc;\">" +
                     "<div style=\"padding-left: 2em;background-color: #e6ffcc;\">1.500";
@@ -27,12 +34,23 @@ public class ScenarioToHTMLTest {
     private ScenarioToHTML scenarioToHTML;
     @Mock
     private ScenarioResult scenarioResult;
+    @Mock
+    private BeforeAfterResult beforeAfterResult;
+    @Mock
+    private Match match;
+    @Mock
+    private Result result;
 
 
     @Before
     public void setup() {
         html = new StringBuilder(TABLE_TAG);
+
         scenarioToHTML = spy(new ScenarioToHTML(scenarioResult));
+
+        when(beforeAfterResult.getMatch()).thenReturn(match);
+        when(beforeAfterResult.getResult()).thenReturn(result);
+        when(beforeAfterResult.getDuration()).thenReturn(DURATION);
     }
 
     @Test
@@ -49,6 +67,22 @@ public class ScenarioToHTMLTest {
         assertFalse(html.toString().contains(EXPECTED_ADD_DURATION_HTML_FOR_PASSED_STEP));
     }
 
+    @Test
+    public void shouldContainAddDurationInAddBeforeAfterResult() {
+        Mockito.doReturn(PASSED_STEP_CSS).when(scenarioToHTML).getCssFormatting(result);
 
+        String actual = scenarioToHTML.addBeforeAfterResult(html, EMPTY_BEFORE_OR_AFTER, beforeAfterResult).toString();
+
+        assertTrue(actual.contains(EXPECTED_ADD_DURATION_HTML_FOR_PASSED_STEP));
+    }
+
+    @Test
+    public void shouldNotContainAddDurationInAddBeforeAfterResultForInvalidStep() {
+        Mockito.doReturn(SKIPPED_STEP_CSS).when(scenarioToHTML).getCssFormatting(result);
+
+        String actual = scenarioToHTML.addBeforeAfterResult(html, EMPTY_BEFORE_OR_AFTER, beforeAfterResult).toString();
+
+        assertFalse(actual.contains(EXPECTED_ADD_DURATION_HTML_FOR_PASSED_STEP));
+    }
 
 }
